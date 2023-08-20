e841ba662667fed698e7a29de9dd122b06d95169
Enki Layman <layman212@gmail.com>
2023-08-20 07:33:57 -0400

    chapter 4 exercise 13

---
+++ chapters/chapter_4_working_with_lists/dimensions.py
@@ -0,0 +1,13 @@
+# In python a list with immutable elements is called a tuple, and it uses () instead of []
+dimensions = (200, 50)
+
+print(dimensions[0])
+print(dimensions[1])
+
+# Tuples are indicated by the commas, the parens are just there to make it look neater, so even if your tuple 
+# just has one element, it needs a trailing comma.
+
+# Tuples can be looped through
+
+# You may not be able to change the values of a tuple, but you can reassign a variable that contains a tuple with a 
+# new one.
---
+++ exercises/chapter_4/buffet.py
@@ -0,0 +1,11 @@
+buffet = ["burger", "fry", "egg"]
+
+for food in buffet:
+    print(food)
+
+buffet[0] = "chicken"
+
+# buffet = ["chicken", "duck", "goose"]
+
+# for food in buffet:
+#     print(food)
