2df8844b6f478dfb9a61abeb7b82aa10a0030a17
Enki Layman <layman212@gmail.com>
2023-08-20 09:21:15 -0400

    learned about checking if a list is empty or not with a conditional statement, and using multiple lists with conditionals/loops.

---
+++ chapters/chapter_5_if_statements/amusement_park.py
@@ -0,0 +1,8 @@
+# Else if is denoted with elif in python
+
+if 0 > 2:
+    print("ues")
+elif 0 < 2:
+    print("no")
+else: 
+    print("noooo")
--- chapters/chapter_5_if_statements/topping.py
+++ chapters/chapter_5_if_statements/topping.py
@@ -20,3 +20,27 @@ if requested_toppings != "anchovies":
 # use parens around the two expressions to make things more readable
 (1 < 9) or (9 < 10)
 
+# if-elif-else is useful if you just want one block of code to run based on a condition, but if you want code to run
+# based on any condition being true just use multiple if statements.
+
+# Checking that a list is not empty before looping through it. If a list is empty it is a falsey value.
+toppings = ["peppers"]
+
+if toppings:
+    for topping in toppings:
+        print(f"{topping.title()} added.")
+else:
+    print("Are you sure you want a plain pizza?")
+
+# Working with multiple lists
+available_toppings = ["pepperoni", "olives", "sausage"]
+requested_toppings = ["pepperoni", "olives", "fries"]
+
+if requested_toppings:
+    for topping in requested_toppings:
+        if topping in available_toppings:
+            print(f"{topping.title()} added.")
+        else:
+            print(f"{topping.title()} is not available.")
+else:
+    print("Are you sure you want a plain pizza?")
